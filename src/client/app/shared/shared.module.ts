7327fedeaf90b495addecd1a163261d7918686f0
Roman Ilyushin <irsick@gmail.com>
2017-01-04 19:19:02 -0500

    Initial commit

---
+++ src/client/app/shared/shared.module.ts
@@ -0,0 +1,41 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { ButtonsModule, DropdownModule, ModalModule, TabsModule } from 'ng2-bootstrap';
+import { ToolbarComponent } from './toolbar/index';
+import { NavbarComponent } from './navbar/index';
+import { NameListService } from './name-list/index';
+
+/**
+ * Do not specify providers for modules that might be imported by a lazy loaded module.
+ */
+
+@NgModule({
+  imports: [
+      CommonModule,
+      RouterModule,
+      ModalModule.forRoot()
+  ],
+  declarations: [
+      ToolbarComponent,
+      NavbarComponent
+  ],
+  exports: [
+      ToolbarComponent,
+      NavbarComponent,
+      CommonModule,
+      FormsModule,
+      RouterModule,
+      ModalModule
+  ]
+})
+export class SharedModule {
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: SharedModule,
+      providers: [NameListService]
+    };
+  }
+}
